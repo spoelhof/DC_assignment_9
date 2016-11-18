ee553607983d0efdd543782369d16067ccf3a17c
spoelhof <spoelhof.jon@ufl.edu>
2016-11-18 12:09:11 -0500

    "Added code to calculate GC content based on Seqs in elf dataset"

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,3 +1,16 @@
 # This script performs a genetic analysis of house elf ear length.
+library(stringr)
 
+#import data
 elf = read.csv("data/houseelf-earlength-dna-data-1.csv", header = T, stringsAsFactors = F)
+
+# Fun. to calculate GC-content (%)
+get_gc_content = function(sequence){
+  Gs <- str_count(tolower(sequence), "g")
+  Cs <- str_count(tolower(sequence), "c")
+  gc_content <- (Gs + Cs)*100 / str_length(sequence) 
+  return(gc_content)
+}
+
+#calculate GC and append to dataset as 'GC'
+(elf$GC = get_gc_content(elf$dnaseq))
