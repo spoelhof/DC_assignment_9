44812645ad29c2e07b98d4afdddb3d6cd31595b6
spoelhof <spoelhof.jon@ufl.edu>
2016-11-18 11:15:09 -0500

    "renamed data file to reflect file number, changed import script"

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,3 +1,3 @@
 # This script performs a genetic analysis of house elf ear length.
 
-elf = read.csv("data/houseelf-earlength-dna-data.csv", header = T, stringsAsFactors = F)
+elf = read.csv("data/houseelf-earlength-dna-data-1.csv", header = T, stringsAsFactors = F)
